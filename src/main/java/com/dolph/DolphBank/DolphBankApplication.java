ae781bad81c04c9c577a853c48e9c56b080da33a
Marko Žeželj <mare.zezelj@icloud.com>
2023-12-11 17:50:37 +0100

    Initial commit

---
+++ src/main/java/com/dolph/DolphBank/DolphBankApplication.java
@@ -0,0 +1,13 @@
+package com.dolph.DolphBank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DolphBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DolphBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dolph/DolphBank/DolphBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dolph.DolphBank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DolphBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
